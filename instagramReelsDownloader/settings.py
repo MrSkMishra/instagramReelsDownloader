73ebd9c7d51153fa0e19b8cbf43ed59c3c973ade
sonu <mishrasonu9211@gmail.com>
2023-08-31 22:16:14 +0530

    database config changed to dbsqlite

--- instagramReelsDownloader/settings.py
+++ instagramReelsDownloader/settings.py
@@ -76,18 +76,25 @@ WSGI_APPLICATION = 'instagramReelsDownloader.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD' : "L1LVY0G6qxVRKSqB9hgl",
+#         'HOST' : "containers-us-west-60.railway.app",
+#         'PORT' : "7352"
+
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD' : "L1LVY0G6qxVRKSqB9hgl",
-        'HOST' : "containers-us-west-60.railway.app",
-        'PORT' : "7352"
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
