fe6f0ee7a462c1c10d1573b373339ffd95bb3f66
sonu <mishrasonu9211@gmail.com>
2023-09-01 16:50:40 +0530

    new update 3

--- reelsapp/downloader.py
+++ reelsapp/downloader.py
@@ -8,46 +8,6 @@ import json
 import re
 from urllib.parse import urlparse, urlunparse
 
-# def ReturnVideoLink(passed):
-#     shortcode = ''
-#     if "reel" in passed or "reels" in passed:
-#         shortcode = re.findall(r"/(reel[s]?)/([A-Za-z0-9_-]+)", passed)[0][1]
-#     elif "p" in passed:
-#         shortcode = re.findall(r"/p/([A-Za-z0-9_-]+)", passed)[0]
-#     else:
-#         return None  
-    
-#     video = f"https://www.instagram.com/p/{shortcode}"
-    
-#     parsed_url = urlparse(video)
-#     clean_url = urlunparse(parsed_url._replace(query=''))
-    
-#     chrome_options = Options()
-#     chrome_options.add_argument("--mute-audio")
-#     chrome_options.add_argument("headless")
-#     chrome_options.add_argument("headless")
-#     chrome_options.add_argument("--autoplay-policy=no-user-gesture-required")
-#     svc = Service(ChromeDriverManager().install())
-#     driver = webdriver.Chrome(service=svc, chrome_options=chrome_options)
-#     driver.delete_all_cookies()
-#     driver.get(clean_url)
-#     delay = 15
-#     soup = ''
-#     while delay >= 0:
-#         try:
-#             soup = driver.execute_script("return document.querySelector('video').outerHTML;")
-#             break
-#         except Exception as e:
-#             print(e)
-#             time.sleep(1)
-#             delay -= 1
-#             print("Sonu")
-#     soup = BeautifulSoup(soup, 'html.parser')
-#     soup = soup.findAll('video')
-#     video_link = soup[0]['src'].replace("&_nc_cat", "&amp:_nc_cat")
-#     return video_link
-
-
 def ReturnVideoLink(passed):
     shortcode = ''
     if "reel" in passed or "reels" in passed:
@@ -62,44 +22,32 @@ def ReturnVideoLink(passed):
     parsed_url = urlparse(video)
     clean_url = urlunparse(parsed_url._replace(query=''))
     
+    # Configure Chrome options
     chrome_options = Options()
     chrome_options.add_argument("--mute-audio")
-    chrome_options.add_argument("headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--autoplay-policy=no-user-gesture-required")
+
+    # Initialize Chrome WebDriver
     svc = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=svc, options=chrome_options)
+    driver.delete_all_cookies()
+    driver.get(clean_url)
     
-    try:
-        driver = webdriver.Chrome(service=svc, chrome_options=chrome_options)
-        driver.delete_all_cookies()
-        driver.get(clean_url)
-        
-        delay = 15
-        soup = None
-        
-        while delay >= 0:
-            try:
-                soup = driver.execute_script("return document.querySelector('video').outerHTML;")
-                break
-            except Exception as e:
-                print(e)
-                time.sleep(1)
-                delay -= 1
-                print("Retrying...")
-        
-        if soup:
-            soup = BeautifulSoup(soup, 'html.parser')
-            video_elements = soup.findAll('video')
-            
-            if video_elements:
-                video_link = video_elements[0]['src'].replace("&_nc_cat", "&amp:_nc_cat")
-                return video_link
-            else:
-                return "No video element found"
-        else:
-            return "Failed to retrieve video content"
+    delay = 15
+    soup = ''
     
-    except Exception as e:
-        return f"Error: {str(e)}"
-    finally:
-        if driver:
-            driver.quit()
+    while delay >= 0:
+        try:
+            soup = driver.execute_script("return document.querySelector('video').outerHTML;")
+            break
+        except Exception as e:
+            print(e)
+            time.sleep(1)
+            delay -= 1
+            print("Sonu")
+    
+    soup = BeautifulSoup(soup, 'html.parser')
+    soup = soup.findAll('video')
+    video_link = soup[0]['src'].replace("&_nc_cat", "&amp:_nc_cat")
+    return video_link
